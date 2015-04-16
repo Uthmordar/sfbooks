42428c863d76bd90e9b5bc275a8452e0166dfb80
Tanguy <tanguyrygodin@gmail.com>
2015-04-16 11:13:48 +0200

    migrations files generation

---
+++ src/AppBundle/Entity/Book.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Book
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="AppBundle\Entity\BookRepository")
+ */
+class Book
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="isbm", type="string", length=255)
+     */
+    private $isbm;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="author", type="string", length=255)
+     */
+    private $author;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="summary", type="string", length=255)
+     */
+    private $summary;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="datePublished", type="date")
+     */
+    private $datePublished;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Get isbm
+     * 
+     * @return type
+     */
+    function getIsbm() {
+        return $this->isbm;
+    }
+
+    /**
+     * 
+     * @param type $isbm
+     * @return \AppBundle\Entity\Book
+     */
+    function setIsbm($isbm) {
+        $this->isbm = $isbm;
+        return $this;
+    }
+    
+    /**
+     * Set title
+     *
+     * @param string $title
+     * @return Book
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string 
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set author
+     *
+     * @param string $author
+     * @return Book
+     */
+    public function setAuthor($author)
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    /**
+     * Get author
+     *
+     * @return string 
+     */
+    public function getAuthor()
+    {
+        return $this->author;
+    }
+
+    /**
+     * Set summary
+     *
+     * @param string $summary
+     * @return Book
+     */
+    public function setSummary($summary)
+    {
+        $this->summary = $summary;
+
+        return $this;
+    }
+
+    /**
+     * Get summary
+     *
+     * @return string 
+     */
+    public function getSummary()
+    {
+        return $this->summary;
+    }
+
+    /**
+     * Set datePublished
+     *
+     * @param \DateTime $datePublished
+     * @return Book
+     */
+    public function setDatePublished($datePublished)
+    {
+        $this->datePublished = $datePublished;
+
+        return $this;
+    }
+
+    /**
+     * Get datePublished
+     *
+     * @return \DateTime 
+     */
+    public function getDatePublished()
+    {
+        return $this->datePublished;
+    }
+}
